d0a5dfd96b66670e83865e5085c2e8e6a12cf019
Tiago Ilieve <tiago.myhro@gmail.com>
2018-11-12 09:04:14 +0000

    common: fix typo

--- common/common.go
+++ common/common.go
@@ -4,7 +4,7 @@ import (
 	"os"
 )
 
-// GetEnv returns the value of an environment variable of a default value if it
+// GetEnv returns the value of an environment variable or a default value if it
 // doesn't exists.
 func GetEnv(name string, defValue string) string {
 	value := os.Getenv(name)
